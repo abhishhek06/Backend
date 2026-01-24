903d7934db40b5327f6579c76e44808f3682d87e
abhishhek06 <abhishekmodak06@gmail.com>
2026-01-24 11:52:09 +0530

    Upolad

--- Day-2/server.js
+++ Day-2/server.js
@@ -1,9 +1,22 @@
 import express from 'express';
 
-const app = express();
+const app = express(); //server instance 
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.listen(3000);
+app.listen(3000, ()=>{
+  console.log('Server is running on http://localhost:3000');
+});
+
+// To run the server, use the command: node server.js
+// npx nodemon server.js 
+
+
+// API: Application Programming Interface: if two software interact with each other it is called API
+
+// REST API: Representational State Transfer API
+
+// protocols: HTTP/HTTPS
+// methods: GET, POST, PUT, PATCH, DELETE
